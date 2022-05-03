8aa295a94ea10cbb9d33b6dbe7b586c64de004df
sojung020924 <sojung020924@naver.com>
2022-05-03 10:12:24 +0900

    init

---
+++ calendar.c
@@ -0,0 +1,6 @@
+#include "diary.h"
+int calender()
+{
+	printf("funcion calendar.\n");
+	return 0;
+}
---
+++ memo.c
@@ -0,0 +1,7 @@
+#include "diary.h"
+int memo()
+{
+	printf("funcion memo.\n");
+	return 0;
+}
+
